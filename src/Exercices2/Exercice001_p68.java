427c41c2cc0bb49e389c776e10d0bcd023e14dbf
unknown <hippolyteaubin@gmail.com>
2023-07-19 14:39:25 +0200

    Finition exercice 1 p.68

--- src/Exercices2/Exercice001_p68.java
+++ src/Exercices2/Exercice001_p68.java
@@ -1,4 +1,22 @@
 package Exercices2;
+//Exercice 1 page 68 du diapo
+import java.util.Scanner;
 
 public class Exercice001_p68 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int age;
+
+        System.out.println("Saisir un age");
+        age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Vous êtes majeur, vous pouvez entrer dans le club");
+        }
+        else {
+            System.out.println("Vous êtes mineur, l'accès vous est refusé.");
+        }
+
+    }
 }
