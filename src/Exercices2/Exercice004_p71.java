2007d4689de90718825cd2483ba523431d02e35e
unknown <hippolyteaubin@gmail.com>
2023-07-20 14:23:59 +0200

    Ajout formatage du caractère saisi,

--- src/Exercices2/Exercice004_p71.java
+++ src/Exercices2/Exercice004_p71.java
@@ -1,34 +1,47 @@
 package Exercices2;
 
+import java.text.Normalizer;
 import java.util.Scanner;
+
+import static java.lang.Character.*;
 
 public class Exercice004_p71 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char inputChar;
+        //tableau contenant les voyelles
         char [] voyelles = {'a', 'e', 'i', 'o', 'u', 'y'};
-        char [] consonnes = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v',
-        'w', 'x', 'z'};
+
+        boolean check = false; // boolean qui servira à sauter ou non la ligne précisant si la lettre saisie est une consonne
 
         System.out.println("Saisir un et un seul caractère :");
         inputChar = scanner.next().charAt(0);
 
-        for(char voyelle : voyelles) {
 
-                if (Character.toLowerCase(inputChar) == voyelle) {
+        //Déclaration d'une chaîne de caractère formatée; récupération du caractère sans accent
+        String s = String.valueOf(toLowerCase(inputChar));
+        s = Normalizer.normalize(s, Normalizer.Form.NFKD);
+        //s.replaceAll("^[a-z]", "");
+        inputChar = s.charAt(0);
+
+        if (isLetter(inputChar)) {
+            for (char voyelle : voyelles) { //parcours du tableau
+                if (voyelle == inputChar) {
                     System.out.println("Le caractère saisi est une voyelle");
-                    break;
-                } else  {
-                    for (char consonne : consonnes) {
-                        if (Character.toLowerCase(inputChar) == consonne) {
-                            System.out.println("Le caractère saisi est une consonne");
-                            break;
-                        }
-                        else System.out.println("Erreur");
-                        break;
-
-                    }
+                    check = true; //il y a correspondance, check est vrai
+                    break; //fin de la boucle si il y a correspondance
                 }
-        }//fin de la boucle voyelles
+                else { //continuer le parcours du tableau jusqu'à trouver la voyelle correspondante
+                    continue;
+
+                }
+            }// sortie de la boucle for; soit le programme est fini, soit il reste à évaluer la cas "consonne"
+            if (check == false) { //récupération du booléen check pour ne pas afficher la ligne ci-dessous automatiquement
+                System.out.println("Le caractère saisi est une consonne");
+            }
+        }
+        else {
+            System.out.println("Le caractère saisi n'est pas une lettre latine");
+        }
     }
 }
