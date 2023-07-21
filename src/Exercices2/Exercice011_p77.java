9cdaa502d422304697386793a2803d0c7a6d472a
unknown <hippolyteaubin@gmail.com>
2023-07-21 14:46:20 +0200

    Ajout de l'exercice 11 p77

---
+++ src/Exercices2/Exercice011_p77.java
@@ -0,0 +1,9 @@
+package Exercices2;
+
+public class Exercice011_p77 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
