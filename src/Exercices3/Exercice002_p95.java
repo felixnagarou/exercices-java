8f40fa524b2af4d6e4ab8eca9a717fff0f0dcc74
unknown <hippolyteaubin@gmail.com>
2023-07-24 15:58:03 +0200

    Modification nom tableau

--- src/Exercices3/Exercice002_p95.java
+++ src/Exercices3/Exercice002_p95.java
@@ -7,19 +7,19 @@ public class Exercice002_p95 {
         Scanner scanner = new Scanner(System.in);
 
         int [] relative = new int[10];
-        int [] absolute = new int[10];
+        int [] positive = new int[10];
 
         for (int i = 0; i <= relative.length -1; i++) {
             System.out.println("Saisir nombre numéro " +  (i+1));
             relative[i] = scanner.nextInt();
             if (relative[i] > 0) {
-                absolute[i] = relative[i];
+                positive[i] = relative[i];
             }
             else {
-                absolute[i] = 0;
+                positive[i] = 0;
             }
 
-            System.out.println(relative[i] + " " + absolute[i]);
+            System.out.println(relative[i] + " " + positive[i]);
 
             }
 
